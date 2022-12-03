0137c5d1c8bb5b5abb75c2615ef949566e48b954
Jappananny <Jappananny@gmail.com>
2022-12-03 20:41:04 +0300

    ДЗ 3.1/1

---
+++ src/main/java/fucking/receipts/receiptsapp/ReceiptsAppApplication.java
@@ -0,0 +1,13 @@
+package fucking.receipts.receiptsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceiptsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fucking/receipts/receiptsapp/ReceiptsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package fucking.receipts.receiptsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
